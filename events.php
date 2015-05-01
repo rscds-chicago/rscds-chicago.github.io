a60820f8970c8a87806d5ebe2763439397223797
Tim Macdonald <tsmacdonald@gmail.com>
2015-04-30 23:10:52 -0500

    Initial commit; shortly before the 2015 Spring SAGM

---
+++ events.php
@@ -0,0 +1,74 @@
+<HTML>
+<HEAD>
+<TITLE>Chicago Branch Events</TITLE>
+<script language="javascript" type="text/javascript">
+<!-- hide code
+function openMap() {
+    newWin = window.open("ucc.html", "mapWin", "toolbar=yes,width=600,height=410");
+}
+function openMap2() {
+    newWin = window.open("irish.html", "mapWin", "toolbar=yes,width=600,height=500");
+}
+function openMap3() {
+    newWin = window.open("images/map.gif", "mapWin", "toolbar=yes,width=600,height=410");
+}
+function openMap4() {
+    newWin = window.open("winnetka.html", "mapWin", "toolbar=yes,width=650,height=450");
+}
+function openUCCP() {
+    newWin = window.open("uccparking.jpg", "mapWin", "toolbar=yes,width=763,height=540");
+}
+// end hiding -->
+</script>
+</HEAD>
+<BODY BACKGROUND="images/bkgd.jpg" text=darkgreen link=coral vlink=darkgreen>
+<CENTER>
+<P>
+<table border=0 cellpadding=0 cellspacing=0>
+<tr><td colspan=2 align=center>
+<img src="images/rscds.gif"></td>
+<td colspan=2 align=center><h1>RSCDS<br>Chicago<br>Branch</h1></td>
+<td colspan=2 align=center>
+<img src="images/20yrpin.gif"></td></tr>
+<tr><td colspan=6 align=center><h3>
+a local association of the Royal Scottish Country Dance Society</h3>
+</td></tr>
+<tr><td><a href="../index.html" noborder><img src="images/rasdhome.gif" border=0></a></td>
+<td><a href="classes.html" noborder><img src="images/rasdclasses.gif" border=0></a></td>
+<td><img src="images/flatevents.gif" border=0></td>
+<td><a href="member.html" noborder><img src="images/rasdmember.gif" border=0></a></td>
+<td><a href="links.html" noborder><img src="images/rasdlinks.gif" border=0></a></td>
+<td><a href="other.html" noborder><img src="images/rasdother.gif" border=0></a></td>
+</tr></table>
+<p align=center><b>Note new URL: rscds-chicago.org</b></p>
+<br><hr><br>
+<table border=0 cellpadding=0 cellspacing=0 width="90%">
+<tr><td align=center>
+<p>
+<h1>Upcoming Branch events:</h1>
+<center>
+<table border=7 cellpadding=5 cellspacing=5 bgcolor=ivory bordercolor=ivory width="90%">
+<?php
+//include("agm.html");
+//include("sagm.html");
+include("springdance.html");
+//include("frolic.html");
+//include("falldance.html");
+//include("nyball.html");
+include("workshop.html");
+?>
+</table>
+</td></tr></table>
+</center>
+<hr>
+<font size=2>
+Webmaster: Sue McKinnell <A HREF="mailto:webmaster@suedan.com"><I>webmaster@suedan.com</I></A>
+<br></font>
+<font size=1>
+Last Update:
+<em><script>
+document.write(document.lastModified);
+</script></em></font>
+
+
+</BODY></HTML>
